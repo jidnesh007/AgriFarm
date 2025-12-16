ba2313956a59a345b0387a8cb114405b00bda42d
Your Name <youremail@example.com>
2025-12-16 14:23:07 +0530

    Local changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,36 @@ import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import FieldList from "./pages/FieldList";
 import FieldDetails from "./pages/FieldDetails";
-import Weather from "./pages/Weather";
+import Weather from "./components/Weather";
 import AiRecommendation from "./components/AiRecommendation";
+import HeroSection from "./pages/Hero";
+import AboutSection from "./components/About";
+import SigroLanding from "./components/SigroLanding";
+import ContactSection from "./Components/Contact";
+import Footer from "./components/Footer";
 
+
+// Landing Page Component - combines all sections
+const LandingPage = () => {
+  return (
+    <>
+      <HeroSection />
+      <AboutSection/>
+       <ContactSection/>
+      <SigroLanding/>
+      <Footer/>
+    </>
+  );
+}; 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<LandingPage />} />
+       <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
+
         <Route path="/fields" element={<FieldList />} />
         <Route path="/field/:id" element={<FieldDetails />} />
         <Route path="/weather" element={<Weather />} />
